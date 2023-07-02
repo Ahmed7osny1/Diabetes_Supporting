6dd5b419e57db746448fc887eff4e6707c604193
Ahmed Hosny <ahmedhosny6688@gmail.com>
2023-07-02 12:54:09 +0300

    Starter App

---
+++ app/src/main/java/com/example/supportingyourdiabetes/ui/PersonFragment.java
@@ -0,0 +1,55 @@
+package com.example.supportingyourdiabetes.ui;
+
+import static android.content.Context.MODE_PRIVATE;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+import com.example.supportingyourdiabetes.R;
+import com.example.supportingyourdiabetes.databinding.FragmentPersonBinding;
+
+public class PersonFragment extends Fragment {
+
+    FragmentPersonBinding binding;
+    SharedPreferences prefs;
+    SharedPreferences.Editor prefsEditor;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        binding = FragmentPersonBinding.inflate(inflater, container, false);
+
+        // Create Shared Preferences
+        prefs = this.requireActivity()
+                .getSharedPreferences("authUser", MODE_PRIVATE);
+        prefsEditor = prefs.edit();
+
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+        binding.yourName.setText(prefs.getString("name",""));
+        binding.yourEmail.setText(prefs.getString("email",""));
+        binding.yourPassword.setText(prefs.getString("password",""));
+        binding.yourPhone.setText(prefs.getString("phone",""));
+
+        binding.addPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view).navigate(
+                        R.id.action_personFragment_to_addPostFragment);
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/supportingyourdiabetes/ui/WelcomeFragment.java
@@ -0,0 +1,67 @@
+package com.example.supportingyourdiabetes.ui;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.fragment.app.Fragment;
+import com.denzcoskun.imageslider.ImageSlider;
+import com.denzcoskun.imageslider.constants.ScaleTypes;
+import com.denzcoskun.imageslider.models.SlideModel;
+import com.example.supportingyourdiabetes.R;
+import com.example.supportingyourdiabetes.databinding.FragmentWelcomeBinding;
+import java.util.ArrayList;
+
+public class WelcomeFragment extends Fragment {
+
+    FragmentWelcomeBinding binding;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        binding = FragmentWelcomeBinding.inflate(inflater, container, false);
+
+        imageSliderSetup();
+
+        return binding.getRoot();
+    }
+
+    private void imageSliderSetup() {
+        ArrayList<SlideModel> imageList = new ArrayList<SlideModel>();
+
+        imageList.add(
+                new SlideModel(
+                        R.drawable.diagnosis,
+                        "Should you be screened for diabetes ?",
+                        ScaleTypes.FIT
+                )
+        );
+        imageList.add(
+                new SlideModel(
+                        R.drawable.lose_weight,
+                        "Lose 5 percent of their weight, Reduce their risk of developing diabetes !",
+                        ScaleTypes.FIT
+                )
+        );
+        imageList.add(
+                new SlideModel(
+                        R.drawable.calendar_green,
+                        "Focus on date and time of medicines and meals !",
+                        ScaleTypes.FIT
+                )
+        );
+        imageList.add(
+                new SlideModel(
+                        R.drawable.breakfast_icon,
+                        "Maintain a healthy diet. ask your nutritionist for guidance.",
+                        ScaleTypes.FIT
+                )
+        );
+
+        ImageSlider imageSlider = binding.imageSlider;
+        imageSlider.setImageList(imageList);
+    }
+
+
+}
